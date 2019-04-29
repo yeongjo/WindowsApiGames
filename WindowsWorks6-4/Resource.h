8784e064aacbee4a6d7b280343ebf25e1e6ebaf4
songchung <syj2466@naver.com>
2019-04-29 19:05:56 +0900

    hi

---
+++ WindowsWorks6-4/Resource.h
@@ -0,0 +1,51 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// WindowsWorks6-4.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_WINDOWSWORKS64_DIALOG       102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSWORKS64              107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSWORKS64              109
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_BUTTON3                     1002
+#define IDC_BUTTON4                     1003
+#define IDC_BUTTON5                     1004
+#define IDC_BUTTON6                     1005
+#define IDC_BUTTON7                     1006
+#define IDC_BUTTON8                     1007
+#define IDC_BUTTON9                     1008
+#define IDC_EDIT1                       1009
+#define IDC_BUTTON17                    1010
+#define IDC_BUTTON19                    1011
+#define IDC_BUTTON10                    1012
+#define IDC_BUTTON11                    1013
+#define IDC_BUTTON12                    1014
+#define IDC_BUTTON13                    1015
+#define IDC_BUTTON14                    1016
+#define IDC_BUTTON15                    1017
+#define IDC_BUTTON16                    1018
+#define IDC_BUTTON18                    1019
+#define IDC_BUTTON20                    1020
+#define IDC_BUTTON21                    1021
+#define IDC_BUTTON22                    1022
+#define IDC_BUTTON23                    1023
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
