a9987c1f8ca7ea277e55011fdccf2b460221bce3
songchung <syj2466@naver.com>
2019-05-08 20:35:23 +0900

    hi

--- SpecialGame1/SpecialGame1.cpp
+++ SpecialGame1/SpecialGame1.cpp
@@ -129,9 +129,14 @@ public:
 
 ObjM ObjM::self;
 
+
+int blockSize = 32;
+
 class Obj {
 public:
 	Pos<float> p;
+	Pos<float> off;
+
 	int size = 32;
 	int h_size = 16;
 	COLORREF color = RGB(20, 20, 20);
@@ -142,6 +147,14 @@ public:
 	virtual void update () {}
 	virtual void render(HDC hdc){
 		renderRect(hdc, (int)p.x, (int)p.y, size, size, RGB(20, 20, 20));
+	}
+	virtual int collObj(Obj *o) {
+		if (o == nullptr) return 0;
+		// 나갔을경우
+		if ((p.x > o->p.x + o->size || p.x + size < o->p.x) ||
+			(p.y > o->p.y + o->size || p.y + size < o->p.y))
+			return 0;
+		return 1;
 	}
 };
 
@@ -177,50 +190,185 @@ public:
 
 class Block : public StaticObj {
 public:
-	virtual void render(HDC hdc) {
+	Block(Pos<float> pos) : StaticObj() {
+		color = RGB(20, 20, 20);
+		p = pos;
+	}
 
+	Block(float x, float y) : StaticObj() {
+		color = RGB(20, 20, 20);
+		p = Pos<float>(x,y);
+	}
+
+	virtual void render(HDC hdc) {
+		renderRect(hdc, p.x + off.x, p.y+ off.y, size, size, color);
 	}
 };
+
+class Enemy; class Player;
+
+class WorldM {
+public:
+	vector<Block *> blocks;
+	vector<Enemy *> enemy;
+	Player *player;
+
+	// 위쪽 ui 때문에
+	int blockYOffset = 64;
+
+	WorldM() {
+		MTimer::create(2000, 1, true, false);
+	}
+
+	// 맵 읽어와서 만듬
+	void createBlocks(int level) {
+		stringstream ss;
+		int yLine = 0;
+		ifstream is;
+
+		switch (level) {
+		case 0:
+			break;
+		case 1:
+		{
+			is.open("map1.txt");
+			break;
+		}
+		case 2:
+			is.open("map2.txt");
+			break;
+		}
+
+		if (is.is_open()) {
+			string s;
+			while (getline(is, s)) {
+				for (size_t i = 0; i < s.size(); i++) {
+					switch (s[i]) {
+					case '0':
+						break;
+					case '1':
+						blocks.push_back(new Block(i * blockSize, blockSize * yLine + blockYOffset));
+						break;
+					}
+				}
+				++yLine;
+			}
+		}
+	}
+
+	void addEnemy();
+
+	void addBlock(Pos<float> pos) {
+		blocks.push_back(new Block(pos));
+	}
+
+	void update() {
+		
+	}
+};
+
+WorldM wm;
 
 class Bullet :public MovableObj {
 public:
 	Pos<float> direc;
-
+	int trailSize = 8;
+	float trailCount = 0;
 	Bullet() : MovableObj(){
 		speed = 8;
+		size = 10;
 	}
 
 	virtual void update() {
 		p += direc*speed;
+		if(trailCount < trailSize)
+		trailCount += speed/trailSize;
 	}
 
 	virtual void render(HDC hdc) {
-		renderCircle(hdc, p.x, p.y);
+		COLORREF t_color = color;
+		Pos<float> t_direc = direc * 6;
+		for (size_t i = 0; i < trailCount; i++) {
+
+			HGDIOBJ  ori = SelectObject(hdc, GetStockObject(DC_PEN));
+			SetDCPenColor(hdc, t_color);
+
+			renderRect(hdc, p.x + off.x - t_direc.x * i, p.y + off.y - t_direc.y * i, size, size, t_color);
+			BYTE r = GetRValue(t_color) + 255 / trailSize;
+			BYTE g = GetGValue(t_color) + 255 / trailSize;
+			BYTE b = GetBValue(t_color) + 255/ trailSize;
+			t_color = RGB(r, g, b);
+
+			SelectObject(hdc, ori);
+		}
 	}
 };
 
-class Enemy : public MovableObj {
-public:
-	Pos<> move_vec;
 
-};
-
-class Player :public MovableObj {
+class Guy : public MovableObj {
 public:
 	int speed = 3;
-	vector<Bullet*> bullet;
+	vector<Bullet *> bullet;
 	Pos<float> move_vec;
 	Pos<float> last_vec;
 	float bulletDelay = 60;
 	int bulletDelayIdx = 0;
 
-	Player() : MovableObj() {
+	Guy() : MovableObj() {
 		// 기본사이즈 32라서 2만큼 더 줄여서 잘보이게 만듬
-		size = size - 2;
-		MTimer::create(bulletDelay, bulletDelayIdx);
+		size = size - 10;
+		color = RGB(20, 20, 20);
 	}
 
-	void inputUpdate () {
+	virtual void render(HDC hdc) {
+		renderRoundRect(hdc, p.x + off.x, p.y + off.y, size, size, 3, 3, color);
+	}
+
+	void shot() {
+		bullet.push_back(new Bullet());
+		auto bul = bullet.back();
+		bul->direc = last_vec;
+		int offgenpos = size / 2 - bul->size / 2;
+		bul->p = p + Pos<float>(offgenpos, offgenpos);
+	}
+
+	void updateBullet() {
+		for (size_t i = 0; i < bullet.size(); i++) {
+			bullet[i]->update();
+		}
+	}
+
+	// 죽음 애니메이션
+	virtual void die() {
+
+	}
+
+	// 정의해서 사용
+	virtual void inputUpdate() {}
+
+	virtual void update() {
+		inputUpdate();
+		//updateBullet();
+		move(move_vec.x, move_vec.y);
+	}
+
+	virtual void move(float x, float y) {
+		Pos<float> t(x, y);
+		p += t;
+	}
+};
+
+
+class Player :public Guy {
+public:
+
+
+	Player() : Guy() {
+		MTimer::create(bulletDelay, bulletDelayIdx);
+		p.set(0, 64);
+	}
+
+	virtual void inputUpdate () {
 		move_vec.set(0, 0);
 		if(KeyM::keys[0])
 			move_vec += Pos<float>(0, -1);
@@ -240,38 +388,101 @@ public:
 			move_vec = move_vec.normalize();
 			last_vec = move_vec;
 			move_vec *= speed;
+			p += move_vec; // 충돌체크를 위해 미리 가있기
+		}
+
+		if (int t = checkIsColl()) {
+			switch (t) {
+			case 1:
+				p -= move_vec; // 원래 위치로
+				move_vec.setZero();
+				break;
+			case 2: // 적이랑 박아서 죽음
+				die();
+				break;
+			}
+		} else {
+			p -= move_vec; // 원래 위치로
 		}
 	}
 
-	void shot() {
-		bullet.push_back(new Bullet());
-		bullet.back()->direc = last_vec;
-	}
-
-	void updateBullet() {
-		for (size_t i = 0; i < bullet.size(); i++) {
-			bullet[i]->update();
+	// 0: not coll
+	// 1: coll wall
+	// 2: coll enemy;
+	int checkIsColl() {
+		for (size_t i = 0; i < wm.blocks.size(); i++) {
+			if (collObj(wm.blocks[i])) return 1; // 벽이랑 박음
 		}
-	}
-
-	virtual void update () {
-		inputUpdate();
-		//updateBullet();
-		move(move_vec.x, move_vec.y);
-	}
-
-	virtual void move (float x, float y) {
-		Pos<float> t (x, y);
-		p += t;
+		for (size_t i = 0; i < wm.enemy.size(); i++) {
+			if (collObj((Obj*)wm.enemy[i])) return 2; // 적 박음
+		}
 	}
 
 	virtual void render (HDC hdc) {
-		renderRoundRect (hdc, p.x, p.y, size, size, 3, 3, RGB (20, 20, 20));
+		renderRoundRect (hdc, p.x + off.x, p.y + off.y, size, size, 3, 3, RGB (20, 20, 20));
+	}
+};
+
+WindowM win;
+
+class UI {
+public:
+	int combe;
+	int score;
+	int level;
+	COLORREF color;
+
+	UI() {
+		color = RGB(50, 50, 50);
+	}
+
+	virtual void render(HDC hdc) {
+		Pos<> size = win.getSize();
+		renderRect(hdc, 0, 0, size.x, 64, color);
+		wstringstream wss;
+		wss << score;
+		TextOut(hdc, 20, 3,_T("SCORE"), 5);
+		TextOut(hdc, 20, 18, wss.str().c_str(), wss.str().size());
+
+		wss.str() = L"";
+		wss.clear();
+		wss << L"+" << combe;
+		TextOut(hdc, 100, 3, wss.str().c_str(), wss.str().size());
+
+		wss.str() = L"";
+		wss.clear();
+		wss << level;
+		TextOut(hdc, size.x-50, 3, wss.str().c_str(), wss.str().size());
 	}
 };
 
 Player player;
-WindowM win;
+
+class Enemy : public Guy {
+public:
+	// 그냥 쓰는거임 플레이어 쫒아오게
+	virtual void inputUpdate() {
+		move_vec = player.p - p;
+		move_vec = move_vec.normalize();
+	}
+};
+
+void WorldM::addEnemy() {
+	auto winSize = win.getSize();
+	enemy.push_back(new Enemy());
+	auto e = enemy.back();
+	int x = rand() % 300;
+	int y = rand() % 300;
+	x = x > 150 ? x + winSize.x - 150 : x;
+	y = y > 150 ? y + winSize.y - 150 : y;
+	e->p.set(x, y);
+}
+
+UI ui;
+
+void initLevel(int level) {
+	wm.createBlocks(level);
+}
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -280,6 +491,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_CREATE:
 		win.init(hWnd);
 		SetTimer(hWnd, 0, 12, NULL);
+		initLevel(1);
 		break;
     case WM_COMMAND:
         {
@@ -318,6 +530,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             HDC hdc = BeginPaint(hWnd, &ps);
 			HDC h = win.prerender(hdc);
 			ObjM::self.render(h);
+			ui.render(h);
 			win.postrender();
 			EndPaint(hWnd, &ps);
         }
